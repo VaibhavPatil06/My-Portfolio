38faab204e81c7a2faea33c8bf8ddbf805183259
vaibhav patil <vaibhavvpatill@gmail.com>
2024-05-01 15:43:25 +0530

    Updates

---
+++ precache-manifest.23f61d412c1d7b5db56ad1a629d2ba92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f510129eb4a1810e157d54f5de4e03ee",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "92356e7db8ab52ba50f9",
+    "url": "/My-Portfolio/static/css/main.09c2e93f.chunk.css"
+  },
+  {
+    "revision": "c72feb6dc286339a449b",
+    "url": "/My-Portfolio/static/js/2.38fec76d.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/My-Portfolio/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "92356e7db8ab52ba50f9",
+    "url": "/My-Portfolio/static/js/main.c9a82572.chunk.js"
+  },
+  {
+    "revision": "2102501848c5a2c746de",
+    "url": "/My-Portfolio/static/js/runtime~main.d6748efb.js"
+  }
+]);
